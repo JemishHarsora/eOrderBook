8258bd2f937050cc738666ad3539403ce15b8658
samip <kothiyasamip@gmail.com>
2021-09-29 09:41:08 +0530

    fix some css

--- resources/views/frontend/partials/best_selling_section.blade.php
+++ resources/views/frontend/partials/best_selling_section.blade.php
@@ -2,7 +2,7 @@
     <section class="mb-4">
         <div class="container-fluid">
             <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
-                <div class="d-flex mb-3 align-items-baseline border-bottom">
+                <div class="d-flex mb-3 align-items-baseline border-bottom category-title">
                     <h3 class="h5 fw-700 mb-0">
                         <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">{{ translate('Best Selling') }}</span>
                     </h3>
