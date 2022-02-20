22bbef52fa265cf8aa088fa6b1e28a6f71cbedf5
samipkothiya <kothiyasamip@gmail.com>
2022-02-19 23:52:30 -1000

    sd

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -132,35 +132,37 @@
     <div class="card-body">
         <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4" data-md-items="3" data-sm-items="2" data-arrows='true'>
             @foreach (filter_products(\App\ProductPrice::with('product')->where('published', 1)->orderBy('num_of_sale', 'desc'))->limit(12)->get() as $key => $product)
-                <div class="carousel-box">
-                    <div class="aiz-card-box border border-light rounded shadow-sm hov-shadow-md mb-2 has-transition bg-white">
-                        <div class="position-relative">
-                            <a href="{{ route('product', $product->slug) }}" class="d-block">
-                                <img
-                                    class="img-fit lazyload mx-auto h-210px"
-                                    src="{{ static_asset('assets/img/placeholder.jpg') }}"
-                                    data-src="{{ uploaded_asset($product->product->thumbnail_img) }}"
-                                    alt="{{ $product->product->getTranslation('name') }}"
-                                    onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';"
-                                >
-                            </a>
-                        </div>
-                        <div class="p-md-3 p-2 text-left">
-                            <div class="fs-15">
-                                <!-- @if(home_base_price($product->id) != home_discounted_base_price($product->id))
-                                    <del class="fw-600 opacity-50 mr-1">{{ home_base_price($product->id) }}</del>
-                                @endif -->
-                                <span class="fw-700 text-primary">{{ home_discounted_base_price($product->id) }}</span>
+                @if($product->product)
+                    <div class="carousel-box">
+                        <div class="aiz-card-box border border-light rounded shadow-sm hov-shadow-md mb-2 has-transition bg-white">
+                            <div class="position-relative">
+                                <a href="{{ route('product', $product->slug) }}" class="d-block">
+                                    <img
+                                        class="img-fit lazyload mx-auto h-210px"
+                                        src="{{ static_asset('assets/img/placeholder.jpg') }}"
+                                        data-src="{{ uploaded_asset($product->product->thumbnail_img) }}"
+                                        alt="{{ $product->product->getTranslation('name') }}"
+                                        onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';"
+                                    >
+                                </a>
                             </div>
-                            <div class="rating rating-sm mt-1">
-                                <!-- {{ renderStarRating($product->product->rating) }} -->
+                            <div class="p-md-3 p-2 text-left">
+                                <div class="fs-15">
+                                    @if(home_base_price($product->id) != home_discounted_base_price($product->id))
+                                        <del class="fw-600 opacity-50 mr-1">{{ home_base_price($product->id) }}</del>
+                                    @endif
+                                    <span class="fw-700 text-primary">{{ home_discounted_base_price($product->id) }}</span>
+                                </div>
+                                <div class="rating rating-sm mt-1">
+                                    {{ renderStarRating($product->product->rating) }}
+                                </div>
+                                <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0">
+                                    <a href="{{ route('product', $product->slug) }}" class="d-block text-reset">{{ $product->product->getTranslation('name') }}</a>
+                                </h3>
                             </div>
-                            <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0">
-                                <a href="{{ route('product', $product->slug) }}" class="d-block text-reset">{{ $product->product->getTranslation('name') }}</a>
-                            </h3>
                         </div>
                     </div>
-                </div>
+                @endif
             @endforeach
         </div>
     </div>
