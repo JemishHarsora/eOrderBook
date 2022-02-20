f8a885e284300d265e878fbe49c888bc012751e9
samipkothiya <kothiyasamip@gmail.com>
2022-02-20 00:47:44 -1000

    sdd

--- resources/views/frontend/partials/best_selling_section.blade.php
+++ resources/views/frontend/partials/best_selling_section.blade.php
@@ -28,53 +28,55 @@
                     }
                     @endphp
                     @foreach ($products as $key => $product)
-                        <div class="carousel-box">
-                            <div class="aiz-card-box border border-light rounded hov-shadow-md my-2 has-transition">
-                                <div class="position-relative">
-                                    <a href="{{ route('product', $product->slug) }}" class="d-block">
-                                        <img
-                                            class="img-fit lazyload mx-auto h-140px h-md-210px"
-                                            src="{{ static_asset('assets/img/placeholder.jpg') }}"
-                                            data-src="{{ uploaded_asset($product->product->thumbnail_img) }}"
-                                            alt="{{  $product->product->getTranslation('name')  }}"
-                                            onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';"
-                                        >
-                                    </a>
-                                    <div class="absolute-top-right aiz-p-hov-icon">
-                                        <a href="javascript:void(0)" onclick="addToWishList({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to wishlist') }}" data-placement="left">
-                                            <i class="la la-heart-o"></i>
+                        @if($product->product)
+                            <div class="carousel-box">
+                                <div class="aiz-card-box border border-light rounded hov-shadow-md my-2 has-transition">
+                                    <div class="position-relative">
+                                        <a href="{{ route('product', $product->slug) }}" class="d-block">
+                                            <img
+                                                class="img-fit lazyload mx-auto h-140px h-md-210px"
+                                                src="{{ static_asset('assets/img/placeholder.jpg') }}"
+                                                data-src="{{ uploaded_asset($product->product->thumbnail_img) }}"
+                                                alt="{{  $product->product->getTranslation('name')  }}"
+                                                onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';"
+                                            >
                                         </a>
-                                        <a href="javascript:void(0)" onclick="addToCompare({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to compare') }}" data-placement="left">
-                                            <i class="las la-sync"></i>
-                                        </a>
-                                        <a href="javascript:void(0)" onclick="showAddToCartModal({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to cart') }}" data-placement="left">
-                                            <i class="las la-shopping-cart"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="p-md-3 p-2 text-left">
-                                    <div class="fs-15">
-                                        @if(home_base_price($product->id) != home_discounted_base_price($product->id))
-                                            <del class="fw-600 opacity-50 mr-1">{{ home_base_price($product->id) }}</del>
-                                        @endif
-                                        <span class="fw-700 text-primary">{{ home_discounted_base_price($product->id) }}</span>
-                                    </div>
-                                    <div class="rating rating-sm mt-1">
-                                        {{ renderStarRating($product->product->rating) }}
-                                    </div>
-                                    <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0 h-35px">
-                                        <a href="{{ route('product', $product->slug) }}" class="d-block text-reset">{{  $product->product->getTranslation('name')  }}</a>
-                                    </h3>
-
-                                    @if (\App\Addon::where('unique_identifier', 'club_point')->first() != null && \App\Addon::where('unique_identifier', 'club_point')->first()->activated)
-                                        <div class="rounded px-2 mt-2 bg-soft-primary border-soft-primary border">
-                                            {{ translate('Club Point') }}:
-                                            <span class="fw-700 float-right">{{ $product->product->earn_point }}</span>
+                                        <div class="absolute-top-right aiz-p-hov-icon">
+                                            <a href="javascript:void(0)" onclick="addToWishList({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to wishlist') }}" data-placement="left">
+                                                <i class="la la-heart-o"></i>
+                                            </a>
+                                            <a href="javascript:void(0)" onclick="addToCompare({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to compare') }}" data-placement="left">
+                                                <i class="las la-sync"></i>
+                                            </a>
+                                            <a href="javascript:void(0)" onclick="showAddToCartModal({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to cart') }}" data-placement="left">
+                                                <i class="las la-shopping-cart"></i>
+                                            </a>
                                         </div>
-                                    @endif
+                                    </div>
+                                    <div class="p-md-3 p-2 text-left">
+                                        <div class="fs-15">
+                                            @if(home_base_price($product->id) != home_discounted_base_price($product->id))
+                                                <del class="fw-600 opacity-50 mr-1">{{ home_base_price($product->id) }}</del>
+                                            @endif
+                                            <span class="fw-700 text-primary">{{ home_discounted_base_price($product->id) }}</span>
+                                        </div>
+                                        <div class="rating rating-sm mt-1">
+                                            {{ renderStarRating($product->product->rating) }}
+                                        </div>
+                                        <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0 h-35px">
+                                            <a href="{{ route('product', $product->slug) }}" class="d-block text-reset">{{  $product->product->getTranslation('name')  }}</a>
+                                        </h3>
+
+                                        @if (\App\Addon::where('unique_identifier', 'club_point')->first() != null && \App\Addon::where('unique_identifier', 'club_point')->first()->activated)
+                                            <div class="rounded px-2 mt-2 bg-soft-primary border-soft-primary border">
+                                                {{ translate('Club Point') }}:
+                                                <span class="fw-700 float-right">{{ $product->product->earn_point }}</span>
+                                            </div>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
