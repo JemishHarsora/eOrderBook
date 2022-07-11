a1d8a47e61acadf760a379898159151f191c40a6
samipkothiya <83601624+samipkothiya@users.noreply.github.com>
2022-07-11 22:26:14 +0530

    Update seller_shop.blade.php

--- resources/views/frontend/seller_shop.blade.php
+++ resources/views/frontend/seller_shop.blade.php
@@ -35,10 +35,10 @@
         $total = 0;
         $rating = 0;
         foreach ($shop->user->priceproduct as $key => $seller_product) {
-            @if($seller_product->product->reviews)
+            if($seller_product->product->reviews){
                 $total += $seller_product->product->reviews->count();
                 $rating += $seller_product->product->reviews->sum('rating');
-            @endif
+            }
         }
     @endphp
 
