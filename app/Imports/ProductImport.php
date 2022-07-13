b812435e57e318f8e685c9dad9ca44c5079e57e2
samipkothiya <83601624+samipkothiya@users.noreply.github.com>
2022-07-13 23:32:16 +0530

    Update ProductImport.php

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -66,8 +66,6 @@ class ProductImport implements ToCollection
 
                     $product->tags = $importData[13];
                     $product->hsn_code = $importData[14];
-
-dd($product,$$importData[11]);
                     $product->save();
 
                     $productPrice = new ProductPrice();
