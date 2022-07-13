fe4be08509312a8dddb29170a096b9b190b4c978
samipkothiya <83601624+samipkothiya@users.noreply.github.com>
2022-07-13 07:27:25 +0530

    Update edit.blade.php

--- resources/views/backend/marketing/flash_deals/edit.blade.php
+++ resources/views/backend/marketing/flash_deals/edit.blade.php
@@ -76,9 +76,9 @@
               
                 @foreach(\App\ProductPrice::with(['product'])->groupBy('product_id')->get() as $product)
                     @php
-                    dd($product->product->getTranslation('name'));
                         $flash_deal_product = \App\FlashDealProduct::where('flash_deal_id', $flash_deal->id)->where('product_id', $product->id)->first();
                     @endphp
+                    {{ $product->product->getTranslation('name') }}
                     <option value="{{$product->id}}" <?php if($flash_deal_product != null) echo "selected";?> >{{ $product->product->getTranslation('name') }}</option>
                 @endforeach
 
